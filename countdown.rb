e2d70de9c9a6045b901e8c1dccb4ff5551073f55
Marianna Katsika <marianna.katsika@gmail.com>
2017-03-08 19:44:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -7,15 +7,10 @@ def countdown(n)
 end
 
 def countdown_with_sleep(n)
-  countdown(n)
-  sleep(5)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep(1)
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
-
-# def countdown_with_sleep(seconds_to_midnight)
-#   while seconds_to_midnight > 0
-#     puts "#{seconds_to_midnight} SECOND(S)!"
-#     sleep(1)
-#     seconds_to_midnight -= 1
-#   end
-#   "HAPPY NEW YEAR!"
-# end
