2e7da23eb7041ee1741950f7d12fcc08aeb2c503
Marianna Katsika <marianna.katsika@gmail.com>
2017-03-08 19:41:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+def countdown(n)
+  while n <= 10 && n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+ "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  countdown(n)
+  sleep(5)
 end
