819213e23a3d61e3d2b27c77a406e81bcd968715
Brett Schules <brettschules@gmail.com>
2017-03-12 19:18:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+
+def countdown(seconds)
+  i = seconds
+  while i >= 1
+  puts "#{i} SECOND(S)!"
+  i -= 1
+end
+   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end
