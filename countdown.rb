156d839c301cb1e36c15cc9f96e02cbe6af59803
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-03-21 02:01:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = num
+  num.times do
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    break if counter == 0
+  end
+
+  if num = 12
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+
+def countdown_with_sleep(num2)
+  sleep(num2)
+  return "Hello"
 end
