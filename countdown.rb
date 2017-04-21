50b335b50eb9fee4d2aaf45a35dccd55371885e3
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-04-21 22:38:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  1.upto(num) do |n| 
+    puts n 
+    sleep 1 
+  end
+  return "Goodnight"
+end 
