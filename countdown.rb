7f1b63b9fc4084f170bc040a6349a290b6bcc38a
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-02 17:43:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  counter = 1
+  while n > 0
+    sleep counter
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
 end
