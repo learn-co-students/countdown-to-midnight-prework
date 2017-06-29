982beb388b50b29c65921b1ca70efae01346a443
joshlacey <williamjoshualacey@gmail.com>
2017-06-28 21:49:18 -0400

    countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+    current = 0
+    while current < integer do
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+        end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+    current = 0
+    while current < integer do
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+        sleep 1
+    end
+    return "HAPPY NEW YEAR!"
+    end
