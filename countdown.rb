7bd5bfffd7f682be722af2c925604e6f5bfbd708
jonopens <jonopenshaw@gmail.com>
2017-08-08 14:35:51 -0400

    while loop and while loop with sleep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(current_seconds)
+  while current_seconds > 0
+    puts "#{current_seconds} SECOND(S)!"
+    current_seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(current_seconds)
+  while current_seconds > 0
+    puts "#{current_seconds} SECOND(S)!"
+    current_seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
