a57fe0638b27edf3f44fedeb85f3360e98f12b92
github username <github email address>
2017-09-02 17:50:24 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(countTime)
+	counter = countTime
+	while counter > 0
+		puts "#{counter} SECOND(S)!"
+		counter -= 1
+	end
+	"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countTime)
+	counter = countTime
+	while counter > 0
+		puts "#{counter} SECOND(S)!"
+		counter -= 1
+		sleep(1)
+	end
+	"HAPPY NEW YEAR!"
 end
