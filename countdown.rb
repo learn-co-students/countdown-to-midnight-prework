7a9ababcf4a7d7c1c77f249b16d9c3005111e650
briecodes <hello@brie.work>
2018-03-13 20:52:26 -0400

    Countdown Methods Added.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(new_int)
+  my_int = new_int
+  while my_int > 0
+    puts "#{my_int} SECOND(S)!"
+    my_int -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(new_int)
+  my_int = new_int
+  while my_int > 0
+    puts "#{my_int} SECOND(S)!"
+    my_int -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
