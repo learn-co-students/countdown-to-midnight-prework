1019fcea1739159f49d2b9b719109a7ba6980ac8
derek chen <derek.g.chen@gmail.com>
2018-03-20 22:17:06 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (second)
+  while second > 0 do
+    puts "#{second} SECOND(S)!"
+    second -= 1
+    countdown_with_sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  while n > 0 do
+    sleep 1
+    n-=1
+  end
+
 end
