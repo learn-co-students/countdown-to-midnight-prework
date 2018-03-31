6de7e73668dfccc19f11681d9adf339b57db7e9d
Eric Razor <eric.razorjr@gmail.com>
2018-03-31 23:42:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+num = 10
+def countdown(num)
+  while num > 0 do
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep .5
+  end
+  return "HAPPY NEW YEAR!"
 end
