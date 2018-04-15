19b8450439dadbc76947c95175b0efa1f263acba
github username <github email address>
2018-04-14 20:13:59 -0400

    Countdown Looping

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
+def countdown (n)
+  n = 10
+  while n < 10
+    return "#{n} SECOND(S)!"
+    n -= 1
+  end
+   return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (seconds)
+  sleep (seconds)
 end
