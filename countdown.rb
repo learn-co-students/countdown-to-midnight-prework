c891116255f51c47a4ea43f90493b0146a766928
github username <github email address>
2018-04-22 17:15:22 -0700

    First Attempt - Method #countdown and #countdown_with_sleep.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int > 0
+    print "#{int} SECOND(S)!"
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  while int > 0
+    print "#{int} SECONDS(S)!"
+    int -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
