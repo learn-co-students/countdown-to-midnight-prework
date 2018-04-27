22c8b8f867a9bfc6267dac7792f69fd78c36c178
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-27 00:32:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time)
+  sleep(5)
+  countdown(time)
 end
