376d99f25c37539a0d0d2f0bdeecf84bdf8a76c4
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-01 01:00:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = number
+  while counter > 0
+  puts "#{number} SECOND(S)!"
+  counter -=
 end
+puts "HAPPY NEW YEAR!"
+end
+
