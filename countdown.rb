624eff625705cde1a910310ffc72509f9f167355
Ramona M <r1mari@yahoo.com>
2018-06-28 11:18:17 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(countdown_output)
+  countdown_output=10
+  while countdown_output>0
+  puts "#{countdown_output} SECOND(S)!"
+  countdown_output-=1
 end
+  return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(sleep)
+  sleep 5
+end
+
+
+
