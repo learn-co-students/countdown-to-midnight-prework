aa74fd22c77125a3a93a1ae818ea78dd4b4a0851
Kelly <devkells00@gmail.com>
2018-07-01 16:41:25 -0500

    Happy New Year

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  n = 10
+  while n <= 10
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    break if n == 0
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep(1)
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
