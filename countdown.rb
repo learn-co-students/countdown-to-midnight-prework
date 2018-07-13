71c78227cee85720b9530a565a764ac6978ce913
Daniel Walz <danielwalz@Daniels-MacBook-Pro.local>
2018-07-12 21:03:56 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  midnight = 0
+  while midnight < time
+    puts "#{time} SECOND(S)!"
+    time -= 1
+end
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time)
+midnight = 0
+while midnight < time
+  puts "#{time} SECOND(S)!"
+  time -= 1
+  sleep(1)
+end
+ "HAPPY NEW YEAR!"
 end
