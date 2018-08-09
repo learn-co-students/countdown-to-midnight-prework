578d72218222968c7d8d9fc755b3c2a303aa4cd0
Chelsea Crowson <chelsea.crowson@me.com>
2018-08-09 17:25:08 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,13 @@
-def countdown_with_sleep(number)
-	sleep (number)
-end
-
 def countdown(number)
 	while number > 0 do puts "#{number} SECOND(S)!"
+		number -= 1
+	end
+	return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+	while number > 0 do puts "#{number} SECOND(S)!"
+		sleep(1)
 		number -= 1
 	end
 	return "HAPPY NEW YEAR!"
