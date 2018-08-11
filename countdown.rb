7fc6b418e1c767fa9b4e78abb186b0adbf793ee7
Nicky Liu <nickyliu91@gmail.com>
2018-08-11 04:53:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,18 @@
 
 def countdown(integer)
   while integer != 0
-    puts "#{integer} SECOND(S)"
+    puts "#{integer} SECOND(S)!"
     integer -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(integer)
+  while integer != 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep 2
   end
   "HAPPY NEW YEAR!"
 end
