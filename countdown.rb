ae93cb83aedc1b269490a19e761324add45c3b47
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 05:59:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  return 'HAPPY NEW YEAR!' if count == 12
+  while count > 0
+    puts "#{count} SECOND(S)!" 
+    count -= 1
+  end 
 end
+
+def countdown_with_sleep(num)
+  sleep(num)
+end 
