85541fd2a5d1053b1477ab902aa8979f0232b7a6
github username <github email address>
2018-10-10 13:49:46 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(counter_number)
+
+while counter_number > 0
+puts "#{counter_number} SECOND(S)!"
+counter_number -= 1
 end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(counter_number)
+  while counter_number > 0
+  puts "#{counter_number} SECOND(S)!"
+  counter_number -= 1
+  sleep(1)
+  end
+    return "HAPPY NEW YEAR!"
+  end
