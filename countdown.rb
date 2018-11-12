a14241f76442eb78a0b82bbd4d612f7e185d9da7
FutureScript <fernandobaxter1@gmail.com>
2018-11-12 13:36:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,17 +2,16 @@
 def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
-    seconds -= 1
+    seconds -=1
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds)
   while seconds > 0
-    puts "#{seconds} SECOND(S)!"
+    puts "#{seconds} SECONDS(S)!"
     seconds -= 1
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
-
