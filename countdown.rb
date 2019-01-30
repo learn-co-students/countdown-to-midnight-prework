78383ebc7ce67ac59e676a98f50c58b148f19d67
Brian Ryu <s.brianryu@gmail.com>
2019-01-30 02:18:28 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,17 +2,17 @@
 
 def countdown(int)
   while int > 0 
-  puts "#{int} SECOND(S)!"
-  int -= 1 
+    puts "#{int} SECOND(S)!"
+    int -= 1 
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(int)
   while int > 0 
-  sleep 1 
-  puts "#{int} SECOND(S)!"
-  int -= 1 
+   sleep 1 
+    puts "#{int} SECOND(S)!"
+      int -= 1 
   end
   return "HAPPY NEW YEAR!"
 end
